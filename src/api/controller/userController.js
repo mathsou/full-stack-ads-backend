afe1c4014a187de7910f82fabfeda42cfe98f01a
Matheo <matheo.rocha@semantix.ai>
2022-06-14 17:59:42 -0300

    crud de usuários

--- src/api/controller/userController.js
+++ src/api/controller/userController.js
@@ -1,12 +1,73 @@
 const connection = require('../../database/connection');
+const md5 = require('md5');
 
 module.exports = {
-    async create(req, res) {
-        res.status(200).json(req.query);
+    async createUser(req, res) {
+        const { body } = req;
+        const createdUser = await connection('users').insert({
+            name: body.name,
+            username: body.username,
+            password: md5(body.password),
+            phone: body.phone,
+            expirationToken: Date.now(),
+            createdAt: Date.now(),
+            updatedAt: Date.now(),
+        }, ['id'])
+        res.status(200).json({ data: { id: createdUser }, msg: "usuário criado com sucesso!" });
     },
 
     async allUsers(req, res) {
         const users = await connection('users')
-        res.status(200).json(users);
-    }
+            .select(
+                'id',
+                'name',
+                'username',
+                'phone',
+                'createdAt',
+                'updatedAt'
+            );
+        res.status(200).json({ data: users });
+    },
+    async oneUser(req, res) {
+        const { params: { id } } = req;
+        const users = await connection('users')
+            .select(
+                'id',
+                'name',
+                'username',
+                'phone',
+                'createdAt',
+                'updatedAt'
+            )
+            .where('id', id)
+            .first();
+        res.status(200).json({ data: users });
+    },
+    async updateUser(req, res) {
+        const { params: { id }, body } = req;
+
+        let updateUser = {
+            name: body.name,
+            username: body.username,
+            phone: body.phone,
+            updatedAt: Date.now(),
+        };
+        if (body.password) {
+            updateUser.password = body.password;
+        }
+        const updatedUser = await connection('users')
+            .update(updateUser, ['id'])
+            .where('id', id)
+
+        res.status(200).json({ data: { id: updatedUser }, msg: "usuário alterado com sucesso!" });
+    },
+
+    async deleteUser(req, res) {
+        const { params: { id } } = req;
+        await connection('users')
+            .where('id', id)
+            .del();
+            
+        res.status(200).json({ msg: "usuário excluído com sucesso!" });
+    },
 }
--- src/api/routes.js
+++ src/api/routes.js
@@ -3,7 +3,10 @@ const routes = express.Router();
 
 const userController = require("./controller/userController");
 
-routes.post("/users", userController.create);
+routes.post("/users", userController.createUser);
 routes.get("/users", userController.allUsers);
+routes.get("/users/:id", userController.oneUser);
+routes.put("/users/:id", userController.updateUser);
+routes.delete("/users/:id", userController.deleteUser);
 
 module.exports = routes;
